423b41178890488b0a9eff6ac440a91959c445bb
Tito Griné <tgrine@singlestore.com>
2024-05-26 19:45:02 +0100

    Implement projects with icons.

--- .eleventy.js
+++ .eleventy.js
@@ -6,4 +6,92 @@ module.exports = function (eleventyConfig) {
   // Watch targets
   eleventyConfig.addWatchTarget("./styles");
   eleventyConfig.addWatchTarget("./scripts");
+
+  eleventyConfig.addShortcode("icon", function (icon) {
+    switch (icon) {
+      case "file":
+        return `
+        <div class="icon file-icon">
+          <div class="folded-corner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          
+          <div>
+            <div class="line">
+              <div class="whitespace"></div>
+              <div class="whitespace"></div>
+              <div class="whitespace"></div>
+              <div class="whitespace"></div>
+            </div>
+            <div class="line">
+              <div class="whitespace"></div>
+              <div class="whitespace"></div>
+              <div class="whitespace"></div>
+            </div>
+            <div class="line">
+              <div class="whitespace"></div>
+              <div class="whitespace"></div>
+              <div class="whitespace"></div>
+            </div>
+          </div>
+        </div>
+        `;
+
+      case "monitor":
+        return `
+    <div class="icon monitor-icon">	  
+      <div class="screen">
+        <div class="line">
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+        </div>
+      
+        <div class="line">
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+        </div>
+      
+        <div class="line">
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+        </div>
+      
+        <div class="line">
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+          <div class="whitespace"></div>
+        </div>
+      
+        <div class="corners">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      
+      <div class="slot"></div>
+      
+      <div class="base"></div>
+
+      <div class="corners">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+	  </div>
+    `;
+    }
+  });
 };
