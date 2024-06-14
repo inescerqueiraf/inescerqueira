7e79b46263276df68d21824c7987f7aced47ec48
Tito Griné <tgrine@singlestore.com>
2024-06-14 20:49:48 +0100

    Make theme persist across pages.

--- scripts/util.js
+++ scripts/util.js
@@ -1,7 +1,16 @@
+const THEME_KEY = "THEME";
+
 window.onload = function () {
   const darkModeButton = document.querySelector("#dark-mode-switcher");
 
-  darkModeButton.addEventListener("click", (event) => {
-    document.documentElement.classList.toggle("dark-mode");
+  const theme = window.localStorage.getItem(THEME_KEY);
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark-mode");
+  }
+
+  darkModeButton.addEventListener("click", () => {
+    const isDarkMode = document.documentElement.classList.toggle("dark-mode");
+
+    window.localStorage.setItem(THEME_KEY, isDarkMode ? "dark" : "light");
   });
 };
